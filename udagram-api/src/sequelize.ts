e02c30d31381308947632f685bd0cc7db051e1bb
roomapp2022 <99840413+drmez2020@users.noreply.github.com>
2022-09-27 03:21:10 +0200

    start

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,14 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(config.postgresDb);
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.port,
 
-//   {
-//   username: config.username,
-//   password: config.password,
-//   database: config.database,
-//   host: config.host,
-
-//   dialect: "postgres",
-//   storage: ":memory:",
-// }
+  dialect: "postgres",
+  storage: ":memory:",
+});
